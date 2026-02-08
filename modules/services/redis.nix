f34b1201fa5b8588fe075550c68e2f07da055116
Kaka Ruto <kr@kakaruto.com>
2026-02-08 03:35:54 +0300

    Fix Redis systemd unit name in services module to match test expectations

--- modules/services/redis.nix
+++ modules/services/redis.nix
@@ -4,7 +4,7 @@ let
   enabled = userState.services.redis or false;
 in
 {
-  services.redis.servers."" = {
+  services.redis.servers.default = {
     enable = enabled;
     port = 6379;
     bind = "127.0.0.1";
